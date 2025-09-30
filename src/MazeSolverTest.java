3fcf63fcc13908b2388e0622c3837989a1fc17b6
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-09-30 16:05:01 -0700

    created helper function for testing, created one basic test

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -84,4 +85,34 @@ public class MazeSolverTest {
     }
 
     // TODO 2: Write good tests for solve
+    @Test
+    void testReachableValidPathOneRegionReturnsPath() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        // Manually create valid path from 1, 3
+        List<Location> validPath = new ArrayList<>();
+        validPath.add(new Location(1, 3));
+        validPath.add(new Location(1, 4));
+        validPath.add(new Location(2, 4));
+        validPath.add(new Location(3, 4));
+
+        // Return from method
+        List<Location> testPath = MazeSolver.solve(1, 3, maze);
+        
+
+        assertTrue(locationsEqual(validPath, testPath));
+    }
+
+    public static boolean locationsEqual(List<Location> list1, List<Location> list2) {
+        if (list1.size() != list2.size()) return false;
+        for (int i = 0; i < list1.size(); i++) {
+            if (!list1.get(i).equals(list2.get(i))) return false;
+        }
+        return true;
+    }
 }
