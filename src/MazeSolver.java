8b0eb0c63f8c7ba40670f6e45ccd317d9efef8b6
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-09-30 10:57:30 -0700

    Added helper for finding neighbors

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 public class MazeSolver {
     public static void main(String[] args) {
@@ -8,6 +8,16 @@ public class MazeSolver {
             {1, 0, 0, 1, 0, 1},
             {1, 0, 0, 1, 3, 1},
         };
+
+        // expected outcome [1,1], [0,2]
+        boolean[][] visited = new boolean[maze1.length][maze1[0].length];
+        List<int[]> neighbors = validNeighbors(0, 1, maze1, visited);
+        for(int[] neighbor : neighbors){
+
+            System.out.println(Arrays.toString(neighbor));
+
+        }
+
 
         int[][] maze2 = {
             {0, 0, 0, 1, 1},
@@ -42,8 +52,52 @@ public class MazeSolver {
      */
     public static boolean reachable(int row, int col, int[][] maze) {
         // We will solve this together as a class.
+
+        // if on 1 or out of bounds = throw new a new IllegalArgumentException
+
+
+
+
+
         return false;
     }
+
+
+    //
+    public static List<int[]> validNeighbors(int startRow, int startCol, int[][]maze, boolean[][] visited){
+
+            int[][] moves = {
+
+                {-1,0}, //up
+                {1,0}, //down
+                {0,-1}, //left
+                {0,1} //right
+
+            }; //end moves
+
+            List<int[]> neighbors = new ArrayList<>();
+
+            for(int[] move : moves){
+
+                int newRow = startRow + move[0];
+                int newCol = startCol + move[1];
+
+
+                if(newRow >= 0 && newRow <maze.length && 
+                  newCol >=0 && newCol <maze[0].length && 
+                  maze[newRow][newCol]!=1 && !visited[newRow][newCol]){
+
+                    neighbors.add(new int []{newRow, newCol});
+
+                  }//end out of bounds + visited checker
+
+            }//end loop for moves
+
+        return neighbors;
+    }//end validNeighbors
+
+
+
 
     /**
      * Returns a valid path from the starting location to a treasure in a maze
