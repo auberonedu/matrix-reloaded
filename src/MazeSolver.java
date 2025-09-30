0f36ae3df89ced7ce85c02b1ad1160d56e6d8565
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-09-30 10:57:12 -0700

    classwork

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+// row/col col col
+// row
+// row
 
 public class MazeSolver {
     public static void main(String[] args) {
@@ -8,6 +13,12 @@ public class MazeSolver {
             {1, 0, 0, 1, 0, 1},
             {1, 0, 0, 1, 3, 1},
         };
+
+        boolean[][] visited = new boolean[maze1.length][maze1[0].length];
+        List<int[]> neighbors = validNeighbors(0, 1, maze1, visited);
+        for(int[] neighbor : neighbors){
+            System.out.println(Arrays.toString(neighbor));
+        }
 
         int[][] maze2 = {
             {0, 0, 0, 1, 1},
@@ -43,6 +54,24 @@ public class MazeSolver {
     public static boolean reachable(int row, int col, int[][] maze) {
         // We will solve this together as a class.
         return false;
+    }
+
+    public static List<int[]> validNeighbors(int startRow, int startCol, int[][] maze, boolean[][] visited){
+        int[][] moves = {
+            {-1, 0},
+            {1,0},
+            {0, 1},
+            {0, -1}
+        };
+        List<int[]> valid = new ArrayList<>();
+        for(int[] move : moves){
+            int newRow = startRow + move[0];
+            int newCol = startCol + move[1];
+            if(newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze[0].length && maze[newRow][newCol] != 1 && !visited[newRow][newCol]){
+                valid.add(new int[]{newRow, newCol});
+            }
+        }
+        return valid;
     }
 
     /**
