9cf010fe174092d01df69359effe80e05863961e
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-09-30 16:11:33 -0700

    added more tests for solve

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -101,11 +101,72 @@ public class MazeSolverTest {
         validPath.add(new Location(2, 4));
         validPath.add(new Location(3, 4));
 
-        // Return from method
+        // Return path from method
         List<Location> testPath = MazeSolver.solve(1, 3, maze);
         
 
         assertTrue(locationsEqual(validPath, testPath));
+    }
+
+    @Test
+    void testStartOnTreasure() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        // Manually create valid path from 1, 3
+        List<Location> validPath = new ArrayList<>();
+        validPath.add(new Location(3, 4));
+
+        // Return path from method
+        List<Location> testPath = MazeSolver.solve(3, 4, maze);
+        
+
+        assertTrue(locationsEqual(validPath, testPath));
+    }
+
+    @Test
+    void testReachableNoValidPathReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 1, 3, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertNull(MazeSolver.solve(2, 1, maze));
+    }
+
+    @Test
+    void testSolveStartingOutOfBoundsThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(5, 6, maze)
+        );
+    }
+
+    @Test
+    void testSolveStartingInWallThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(0, 0, maze)
+        );
     }
 
     public static boolean locationsEqual(List<Location> list1, List<Location> list2) {
