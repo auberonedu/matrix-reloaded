f5eb80ece2f609f33d47eb3ed00d26857fe1b581
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-01 10:54:40 -0700

    adding tests and fixing reachable

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class MazeSolver {
     public static void main(String[] args) {
         Location myLocation = new Location(4,7);
         System.out.println(myLocation.row());
         Location location2 = new Location(5,3);
-        
+
         // int[][] maze1 = {
         //     {1, 0, 0, 0, 1, 1},
         //     {0, 0, 1, 0, 0, 0},
@@ -66,7 +67,8 @@ public class MazeSolver {
     }
 
     public static List<int[]> validNeighbors(int startRow, int startCol, int[][] maze, boolean[][] visited){
-        int[][] directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        int[][] directions =
+        {{-1,0}, {1,0}, {0,-1}, {0,1}};
         List<int[]> neighbors = new ArrayList<>();
         for(int[] dir : directions){
             int newRow = startRow + dir[0];
@@ -117,6 +119,59 @@ public class MazeSolver {
     public static List<Location> solve(int row, int col, int[][] maze) {
         // You will solve this with a partner
         // Please do not begin work on this until directed to!
+        if(row < 0 || col < 0 || row >= maze.length || col >= maze[0].length){
+            throw new IllegalArgumentException("Out of bounds location: " + row + ", " + col);
+        }
+        if(maze[row][col] == 1) throw new IllegalArgumentException("location starts in wall");
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        Stack<Location> path = new Stack<>();
+        if(solve(row, col, maze, visited, path)) return path;
+
         return null;
     }
+
+    private static boolean solve(int row, int col, int[][] maze, boolean[][] visited, Stack<Location> path){
+        
+        visited[row][col] = true;
+        path.add(new Location(row, col));
+        if(maze[row][col] == 3) return true;
+        for(Location loc: validLocations(row, col, maze, visited)){
+            
+            if(solve(loc.row(), loc.col(), maze, visited, path)){
+                return true;
+            }else{
+                path.pop();
+            }
+        }
+        return false;
+    }
+
+
+    public static List<Location> validLocations(int startRow, int startCol, int[][] maze, boolean[][] visited){
+        List<Location> directions = new ArrayList<>();
+        directions.add(new Location(-1,0));
+        directions.add(new Location(1,0));
+        directions.add(new Location(0,-1));
+        directions.add(new Location(0,1));
+        List<Location> neighbors = new ArrayList<>();
+
+        for(Location dir : directions){
+            int newRow = startRow + dir.row();
+            int newCol = startCol + dir.col();
+
+            if(newRow >= 0 &&
+                newRow < maze.length &&
+                newCol >= 0 &&
+                newCol < maze[0].length &&
+                maze[newRow][newCol] != 1 &&
+                !visited[newRow][newCol]){
+                    neighbors.add(new Location(newRow, newCol));
+                }
+            
+        }   
+        return neighbors;
+    }
+
+
+
 }
--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 
 public class MazeSolverTest {
@@ -56,7 +60,127 @@ public class MazeSolverTest {
     }
 
     // TODO 1: Write more tests for reachable
-    
+    @Test
+    void testReachableStartingOffGridThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(8, 0, maze)
+        );
+    }
+
+    @Test
+    void testReachableStartTreasureTrue() {
+        int[][] maze = {
+            {3, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 1, 1, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertTrue(MazeSolver.reachable(0, 0, maze));
+    }
+    @Test
+    void testReachableValidLongPathTrue() {
+        int[][] maze = {
+            {0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 0, 1, 0},
+            {3, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 0},
+        };
+
+        assertTrue(MazeSolver.reachable(3, 2, maze));
+    }
 
     // TODO 2: Write good tests for solve
+
+    @Test
+    void testReachableOnePath() {
+        int[][] maze = {
+            {0, 1, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 3, 0, 1, 0, 1},
+            {1, 0, 0, 1, 0, 1},
+        };
+        List<Location> path = new ArrayList<>();
+        Location p1 = new Location(0,0);
+        Location p2 = new Location(1,0);
+        Location p3 = new Location(1,1);
+        Location p4 = new Location(2,1);
+        path.add(p1);
+        path.add(p2);
+        path.add(p3);
+        path.add(p4);
+
+       assertEquals(path, MazeSolver.solve(0, 0, maze));
+    }
+
+    @Test
+    void testSolveTwoPaths() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 3},
+            {0, 0, 1, 1, 1},
+            {1, 1, 1, 1, 1}
+        };
+        List<Location> path = new ArrayList<>();
+        Location p1 = new Location(1,3);
+        Location p2 = new Location(1,4);
+        Location p3 = new Location(2,4);
+        
+        path.add(p1);
+        path.add(p2);
+        path.add(p3);
+
+        List<Location> path2 = new ArrayList<>();
+        Location l1 = new Location(1,3);
+        Location l2 = new Location(2,3);
+        Location l3 = new Location(2,4);
+        path2.add(l1);
+        path2.add(l2);
+        path2.add(l3);
+
+        List<Location> actual = MazeSolver.solve(1, 3, maze);
+        if(path == actual){
+            assertEquals(path, actual);
+        }else{
+            assertEquals(path2, actual);
+        }
+    }
+
+
+    @Test
+    void testSolveStartingInWallThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.solve(0, 0, maze)
+        );
+    }
+
+    @Test
+    void testSolveNoValidPathReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 1, 3, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertEquals(MazeSolver.solve(2, 1, maze), null);
+    }
+
 }
