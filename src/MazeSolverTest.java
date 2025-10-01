f8b557fdc970503ddde1147f28f55bb7444fcbf8
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-01 16:57:11 -0700

    added tests for solve

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MazeSolverTest {
@@ -71,7 +73,7 @@ public class MazeSolverTest {
         assertFalse(MazeSolver.reachable(0, 0, maze));
     }
 
-     @Test
+    @Test
     void testReachableStartOnTreasureReturnsTrue() {
         int[][] maze = {
             {1, 1, 1, 1, 1, 1},
@@ -141,6 +143,150 @@ public class MazeSolverTest {
     }
 
     // TODO 2: Write good tests for solve
-
     
+    @Test
+    void testSolveStartingInWallThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.solve(0, 0, maze)
+        );
+    }
+
+    @Test
+    void testSolveStartingColumnOutOfBoundsThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 0},
+            {1, 0, 0, 1, 3, 0},
+            {0, 1, 0, 0, 0, 0},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.solve(1, 8, maze)
+        );
+    }
+
+    @Test
+    void testSolveStartingRowOutOfBoundsThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 0},
+            {1, 0, 0, 1, 3, 0},
+            {0, 1, 0, 0, 0, 0},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.solve(6, 2, maze)
+        );
+    }
+
+    @Test
+    void testSolveStartOnTreasureReturnsCorrectList() {
+        int[][] maze = {
+            {1, 0, 0, 0},
+            {0, 0, 1, 0},
+            {0, 1, 0, 1},
+            {0, 0, 0, 3},
+        };
+
+        List<Location> expectedPath = List.of( 
+            new Location(3, 3) 
+        );
+
+        List<Location> actualPath = MazeSolver.solve(3, 3, maze);
+        assertEquals(expectedPath, actualPath);
+    }
+
+    @Test
+    void testSolveOneValidPathReturnsCorrectList() {
+        int[][] maze = {
+            {1, 0, 0, 0},
+            {0, 0, 1, 0},
+            {0, 1, 0, 1},
+            {0, 0, 0, 3},
+        };
+
+        List<Location> expectedPath = List.of(
+            new Location(0, 2),
+            new Location(0, 1),
+            new Location(1, 1),
+            new Location(1, 0),
+            new Location(2, 0),
+            new Location(3, 0),
+            new Location(3, 1),
+            new Location(3, 2), 
+            new Location(3, 3) 
+        );
+
+        List<Location> actualPath = MazeSolver.solve(0, 2, maze);
+        assertEquals(expectedPath, actualPath);
+    }
+
+    @Test
+    void testSolveTwoValidPathsReturnsCorrectList() {
+        int[][] maze = {
+            {0, 0, 0, 3},
+            {1, 1, 0, 1},
+            {0, 0, 0, 0},
+            {0, 1, 1, 0}
+        };
+
+        List<Location> expectedPathA = List.of(
+            new Location(3,0),
+            new Location(2,0),
+            new Location(2,1),
+            new Location(2,2),
+            new Location(1,2),
+            new Location(0,2),
+            new Location(0,3)
+        );
+
+        List<Location> expectedPathB = List.of(
+            new Location(3,0),
+            new Location(2,0),
+            new Location(2,1),
+            new Location(2,2),
+            new Location(2,3),
+            new Location(1,2),
+            new Location(0,2),
+            new Location(0,3)
+        );
+
+        List<Location> actualPath = MazeSolver.solve(3, 0, maze);
+        assertTrue(actualPath.equals(expectedPathA) || actualPath.equals(expectedPathB));
+    }
+
+    @Test
+    void testSolveNoValidPathReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 0},
+            {0, 1, 1, 0},
+            {0, 1, 1, 1},
+            {0, 0, 1, 3},
+        };
+        
+        List<Location> actualPath = MazeSolver.solve(0, 0, maze);
+        assertNull(actualPath);
+    }
+
+    @Test
+    void testSolveNoTreasureReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+        };
+
+        List<Location> actualPath = MazeSolver.solve(0, 0, maze);
+        assertNull(actualPath);
+    }
 }
