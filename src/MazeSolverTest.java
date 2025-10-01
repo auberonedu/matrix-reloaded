b920a120e005d6e7b357ffb959bb990d1020cef9
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-01 12:35:38 -0700

    added additional tests for reachable

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -57,5 +57,90 @@ public class MazeSolverTest {
 
     // TODO 1: Write more tests for reachable
 
+    @Test
+    void testReachableNoTreasureReturnsFalse() {
+        int[][] maze = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+        };
+
+        assertFalse(MazeSolver.reachable(0, 0, maze));
+    }
+
+     @Test
+    void testReachableStartOnTreasureReturnsTrue() {
+        int[][] maze = {
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1},
+            {1, 3, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1}
+        };
+
+        assertTrue(MazeSolver.reachable(2, 1, maze));
+    }
+
+    @Test
+    void testReachableStartingColumnOutOfBoundsThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 0},
+            {1, 0, 0, 1, 3, 0},
+            {0, 1, 0, 0, 0, 0},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(1, 8, maze)
+        );
+    }
+
+    @Test
+    void testReachableStartingRowOutOfBoundsThrowsIllegalArgumentException() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 0},
+            {1, 0, 0, 1, 3, 0},
+            {0, 1, 0, 0, 0, 0},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(6, 2, maze)
+        );
+    }
+
+    @Test
+    void testReachableMultipleTreasuresValidPathToOneReturnsTrue() {
+        int[][] maze = {
+            {0, 1, 3, 1, 1, 1},
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0},
+            {1, 0, 1, 3, 1, 0},
+            {1, 0, 1, 1, 1, 0},
+            {1, 0, 0, 0, 0, 0}
+        };
+        assertTrue(MazeSolver.reachable(0, 0, maze));
+    }
+    
+    @Test
+    void testReachableMultipleTreasuresValidPathToBothReturnsTrue() {
+        int[][] maze = {
+            {0, 1, 3, 0, 1, 1},
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0},
+            {1, 0, 1, 3, 1, 0},
+            {1, 0, 1, 1, 1, 0},
+            {1, 0, 0, 0, 0, 0}
+        };
+        assertTrue(MazeSolver.reachable(0, 0, maze)); 
+    }
+
     // TODO 2: Write good tests for solve
+
+    
 }
