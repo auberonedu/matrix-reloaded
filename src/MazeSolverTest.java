458d73382d3ad2bc9b19e253f8aaa5cdd48b0db8
Butenkite <butenkite@gmail.com>
2025-10-01 20:01:50 -0700

    initial tests

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MazeSolverTest {
@@ -82,4 +84,43 @@ public class MazeSolverTest {
     }
 
     // TODO 2: Write good tests for solve
+
+    @Test
+    void testSmallRoundAboutTrue(){
+        int[][] maze = {
+            {0, 0, 0, 0},
+            {1, 1, 1, 0},
+            {3, 0, 0, 0}
+        };
+        assertEquals(List.of(
+            new Location(0, 0),
+            new Location(0, 1),
+            new Location(0, 2),
+            new Location(0, 3),
+            new Location(1, 3),
+            new Location(2, 3),
+            new Location(2, 2),
+            new Location(2, 1),
+            new Location(2, 0)
+        ), MazeSolver.solve(0, 0, maze));
+    }
+    @Test
+    void testSmallRoundAboutFalse(){
+        int[][] maze = {
+            {0, 0, 0, 0},
+            {1, 1, 1, 1},
+            {3, 0, 0, 0}
+        };
+        assertNotEquals(List.of(
+            new Location(0, 0),
+            new Location(0, 1),
+            new Location(0, 2),
+            new Location(0, 3),
+            new Location(1, 3),
+            new Location(2, 3),
+            new Location(2, 2),
+            new Location(2, 1),
+            new Location(2, 0)
+        ), MazeSolver.solve(0, 0, maze));
+    }
 }
