3805eee30f76e25d9735a095b8280e5a1200a7a5
JesseChum <jessechum@icloud.com>
2025-10-01 22:19:25 -0700

    Created more tests for both TODO1 and TODO2. All test successfully pass after following example code from above

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MazeSolverTest {
@@ -56,6 +58,75 @@ public class MazeSolverTest {
     }
 
     // TODO 1: Write more tests for reachable
+    @Test
+    void testReachableNoTreasureReturnsFalse() {
+        int[][] maze = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+        };
+
+        assertFalse(MazeSolver.reachable(0, 0, maze));
+    }
+
+     @Test
+    void testReachableMultipleTreasuresValidPathToOneReturnsTrue() {
+        int[][] maze = {
+            {0, 1, 3, 1, 1, 1},
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0},
+            {1, 0, 1, 3, 1, 0},
+            {1, 0, 1, 1, 1, 0},
+            {1, 0, 0, 0, 0, 0}
+        };
+        assertTrue(MazeSolver.reachable(0, 0, maze));
+    }
 
     // TODO 2: Write good tests for solve
+
+     @Test
+    void testSolveableNoTreasureReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1},
+        };
+
+        List<Location> path = MazeSolver.solveable(0, 0, maze);
+        assertNull(path);
+    }
+
+      @Test
+    void testSolveableNoValidPathReturnsNull() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 1, 3, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        List<Location> path = MazeSolver.solveable(2, 1, maze);
+        assertNull(path);
+    }
+
+    void testSolveableValidPathOneRegionReturnsPath() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        List<Location> path = MazeSolver.solveable(3, 2, maze);
+        assertNotNull(path);
+        assertEquals(new Location(3, 2), path.get(0));
+        assertEquals(new Location(3, 4), path.get(path.size()-1));
+    }
 }
