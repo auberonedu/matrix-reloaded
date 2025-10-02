2f5af13468d26cceb056c5766d2f3ecc206d47b9
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-01 23:14:37 -0700

    added a few more tests for reachable and solve and all passed

--- src/MazeSolverTest.java
+++ src/MazeSolverTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MazeSolverTest {
@@ -57,6 +59,61 @@ public class MazeSolverTest {
 
     // TODO 1: Write more tests for reachable
 
+    @Test
+    void testReachableStartingOutOfBounds() {
+        int[][] maze = {
+            {1, 0, 0, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 3, 1},
+        };
+
+        assertThrows(IllegalArgumentException.class,
+            ()->MazeSolver.reachable(10, 7, maze)
+        );
+    }
+
+    @Test
+    void testReachableMultipleReachable() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 0, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 3, 1, 3, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertTrue(MazeSolver.reachable(2, 1, maze));
+    }
+
+    @Test
+    void testReachableOnlyOneReachable() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 3, 1, 3, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertTrue(MazeSolver.reachable(2, 1, maze));
+    }
+
+    @Test
+    void testReachableNoTreasure() {
+        int[][] maze = {
+            {0, 0, 0, 1, 1},
+            {0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 1},
+            {0, 0, 1, 0, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        assertFalse(MazeSolver.reachable(2, 1, maze));
+    }
+
+
+
     // TODO 2: Write good tests for solve
 
     @Test
@@ -103,11 +160,14 @@ public class MazeSolverTest {
     void testSolveValidPath() {
         int[][] maze = {
             {1, 0, 0, 0, 1, 1},
-            {0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 0},
             {1, 0, 0, 1, 0, 1},
             {1, 0, 0, 1, 3, 1},
         };
 
-        assertNull(MazeSolver.solve(2, 1, maze));  
+        List<Location> path = MazeSolver.solve(0, 1, maze);
+        assertNotNull(path);
+        assertEquals(3, path.get(path.size()-1).row());
+        assertEquals(4, path.get(path.size()-1).col());
     }
 }
